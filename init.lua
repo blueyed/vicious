50dc101de0c5d78811f28d20691f001cee328469
Daniel Hahler <git@thequod.de>
2015-07-19 17:47:44 +0200

    Add vicious.change_timer: a helper to change the timeout
    This is useful in case you want to change the timeout value for a
    registered widget, e.g. from the format callback function.
    
    I am using this to have a shorter timeout interval for the temperature
    widget when its value is above some threshold.
    
    Signed-off-by: Daniel Hahler <git@thequod.de>

--- init.lua
+++ init.lua
@@ -306,6 +306,17 @@ function vicious.call(myw, format, warg)
 end
 -- }}}
 
+-- {{{ Change the timer of a registered widget.
+function vicious.change_timer(reg, timer_)
+  if not reg then return end
+  if timer_ ~= reg.timer then
+    vicious.unregister(nil, true, reg)
+    reg.timer = timer_
+    regregister(reg)
+  end
+end
+-- }}}
+
 return vicious
 
 -- }}}
